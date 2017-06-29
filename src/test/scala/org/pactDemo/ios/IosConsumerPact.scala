1a1b87381e5e0395a2aaa5d9c088dfe2507ea546
Phil <phil.rice@iee.org>
2017-06-29 08:44:34 +0200

    Brokencode

--- src/test/scala/org/pactDemo/ios/IosConsumerPact.scala
+++ src/test/scala/org/pactDemo/ios/IosConsumerPact.scala
@@ -13,23 +13,55 @@ class IosConsumerPact extends FunSpec with Matchers {
   //  implicit val formats = DefaultFormats
 
   describe("Connecting to the Provider service") {
+    //    Given the token is valid for that ID
+    //      When I send
+    //    /token/id/1
+    //    With the header:
+    //      Authorisation token: 1923879ladskfhsdlfkj
+    //    Then I get status 200 and “{“token” : “ok”}
+    //    Given the token is in valid for that ID
 
-    it("should be able to fetch results") {
+    it("should be able to detect valid tokens") {
       forgePact
         .between("Ios")
         .and("Provider")
         .addInteraction(
           interaction
             .description("Fetching results")
-            //            .given("Results: Bob, Fred, Harry")
-            .uponReceiving("/id/1")
-            .willRespondWith(200, """{"id":"1"}""")
+            .given("token '7899-valid-for-id-1-token'  is valid for id 1")
+            .uponReceiving("/token/1")
+            //add the header
+            .willRespondWith(200, """{"token":"valid"}""")
         )
         .runConsumerTest {
           mockConfig =>
-            val client = new FinatraClient(mockConfig.host, mockConfig.port, x => x)
-            client(1).await
+            val client = new AuthenticationClient(mockConfig)
+            val request = AuthenticationRequest("1","7899-valid-for-id-1-token")
+            client(request).await shouldBe AuthenticationValid
         }
     }
   }
+
+  it("should be able to detect invalid tokens") {
+    forgePact
+      .between("Ios")
+      .and("Provider")
+      .addInteraction(
+        interaction
+          .description("Fetching results")
+          .given("token '123-invalid-for-id-1-token'  is invalid for id 1")
+          .uponReceiving("/token/1")
+          //add the header
+          .willRespondWith(200, """{"token":"invalid"}""")
+      )
+      .runConsumerTest {
+        mockConfig =>
+          val client = new AuthenticationClient(mockConfig)
+          // request => Future[Response]
+          val request = AuthenticationRequest("1", "123-invalid-for-id-1-token")
+          client(request).await shouldBe AuthenticationInvalid
+      }
+  }
+}
+
 }
